e1f261f05eef17ef8972cdca023ff6490ca39864
Jacek Milewski <jacek.milewski@orange.com>
2017-06-26 13:35:23 +0200

    greeting controller

---
+++ src/main/java/com/orange/kermit/DemoController.java
@@ -0,0 +1,25 @@
+package com.orange.kermit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.invoke.MethodHandles;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class DemoController {
+
+    private Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @RequestMapping(path = "greeting", method = GET)
+    public String greetingController(
+            @RequestParam(value = "user", required = false, defaultValue = "Developer") String user
+    ){
+        log.info("Returning greeting for: {}", user);
+        return "Hello, " + user + "!";
+    }
+}
